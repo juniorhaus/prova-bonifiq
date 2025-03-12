eb7b08113a91be4cd79bbde928bb8c68bff6df3d
unknown <p.prinspo@gmail.com>
2025-03-12 16:18:55 -0300

    Parte2Controller 1

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,21 +1,30 @@
-﻿using ProvaPub.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using ProvaPub.Models;
 using ProvaPub.Repository;
 
 namespace ProvaPub.Services
 {
-	public class ProductService
-	{
-		TestDbContext _ctx;
+    public class ProductService
+    {
+        TestDbContext _ctx;
 
-		public ProductService(TestDbContext ctx)
-		{
-			_ctx = ctx;
-		}
+        public ProductService(TestDbContext ctx)
+        {
+            _ctx = ctx;
+        }
 
-		public ProductList  ListProducts(int page)
-		{
-			return new ProductList() {  HasNext=false, TotalCount =10, Products = _ctx.Products.ToList() };
-		}
+        public ProductList ListProducts(int pageIndex)
+        {
+            const int pageSize = 10;
 
-	}
+            var products = _ctx.Products;
+
+            var count = products.Count();
+            var totalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            var hasNext = pageIndex < totalPages;
+
+            return new ProductList() { HasNext = hasNext, TotalCount = totalPages, Products = products.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList() };
+        }
+    }
 }
